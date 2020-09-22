90f1af59399363a157b4197faa853fe3aab69f9c
unperknown <unperknown@icloud.com>
2020-09-23 08:29:15 +0900

    feat: apollo connection

--- gsm-pay/src/index.js
+++ gsm-pay/src/index.js
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
-ReactDOM.render(<Root />, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const cache = new InMemoryCache();
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GSM_PAY_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const session = localStorage.getItem('session');
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: session ?? '',
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Root />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
+
 serviceWorker.unregister();
